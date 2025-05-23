f7f142cbf548efb0e4d1b5e4e43c5a4a568e273a
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2025-05-23 19:35:47 +0100

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -4,16 +4,20 @@ import requests as rq
 import tkinter as tk
 from tkinter import messagebox as msg
 from PIL import ImageTk, Image
+from io import BytesIO
 import os
 
 root = tk.Tk()
 root.wm_resizable(width=False, height=False)
 root.title("Pokédex👌")
+root.config(bg="#e82b00")
 
-titl = tk.Label(text="🔎The Pokédex", font="Fixedsys 18 bold")
+titl = tk.Label(text="🔎The Pokédex", font="Fixedsys 18 bold", bg="#e82b00", fg="#80200a")
 titl.pack()
 search = tk.Entry(width=20, font="Fixedsys 12", justify="center")
 search.pack(pady=5)
-fins = tk.Button(bg="green", fg="#FFFFFF", text="Search...", font="Fixedsys")
-fins.pack(pady=10)
+find = tk.Button(bg="green", fg="#FFFFFF", text="Search...", font="Fixedsys")
+find.pack(pady=10)
+imglabel = tk.Label(root, bg="#e82b00", width=20, height=10)
+imglabel.pack(pady=5)
 root.mainloop()
